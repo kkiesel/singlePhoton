e3df2edb9863789b3724e7dac9b72434dd90a620
kiesel <>
2013-05-02 08:33:04 +0000

    added setter and getter functions for gen information

--- TreeWriter/TreeObjects.h
+++ TreeWriter/TreeObjects.h
@@ -11,11 +11,21 @@ class Particle {
 
 class Photon : public Particle {
 	public:
+		bool isGenPhoton() { return genInformationBit == 1; }
+		bool isGenElectron() { return genInformationBit == 2; }
+		bool isGenJet() { return genInformationBit == 3; }
+
+		void matchGenPhoton() { genInformationBit = 1; }
+		void matchGenElectron() { genInformationBit = 2; }
+		void matchGenJet() { genInformationBit = 3; }
+
 		float ptJet;
 		float r9, sigmaIetaIeta, hadTowOverEm;
 		float chargedIso, neutralIso, photonIso;
 		bool conversionSafeVeto;
 		int pixelseed;
+	private:
+		int genInformationBit;
 };
 
 class Jet : public Particle{
