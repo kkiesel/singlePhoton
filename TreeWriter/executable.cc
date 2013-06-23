17b34541f06ee293173e5666ffeea1d0f3472c3e
Knut <maximilian.kiesel@rwth-aachen.de>
2013-06-23 13:18:32 +0200

    removed command line options, and included automatic data/mc identification

--- TreeWriter/executable.cc
+++ TreeWriter/executable.cc
@@ -1,59 +1,37 @@
 #include "treeWriter.h"
 
-char* getCmdOption(char ** begin, char ** end, const std::string & option) {
-	char ** itr = std::find(begin, end, option);
-	if (itr != end && ++itr != end)
-		return *itr;
-	return 0;
-}
-
-bool cmdOptionExists(char** begin, char** end, const std::string& option) {
-	return std::find(begin, end, option) != end;
-}
-
-void printHelp() {
-	std::cout << "Usage: ./executable [-h] [-MC] -o outFileName.root -i inputFileName1.root inputFileName2.root ..." << std::endl;
-}
-
 int main( int argc, char** argv ) {
 
-	if( cmdOptionExists( argv, argv+argc, "-h" ) ) {
-		printHelp();
-		return 0;
-	}
-
-	bool isMC = cmdOptionExists( argv, argv+argc, "-MC" ); // false by default
-
-	char * filename = getCmdOption(argv, argv + argc, "-o");
-	std::string outputFileName;
-	if (filename) {
-		outputFileName = filename;
-	} else {
-		std::cout << "No output filename specified." << std::endl;
-		printHelp();
+	if( argc < 2 ) {
+		std::cout << "usage: ./execute outputFileName.root input1.root input2.root ..." << std::endl;
 		return 1;
 	}
+	std::string outputFileName = argv[1];
 
 	TChain *inputTree = new TChain("susyTree");
-	char ** itr = std::find(argv, argv+argc, (std::string)"-i");
-	while (itr != argv && ++itr != argv+argc)
-		inputTree->Add( *itr );
+	for( int i=2; i<argc; ++i)
+		inputTree->Add( argv[i] );
 
 	std::cout << "Write to output file \"" << outputFileName << "\"" << std::endl;
 
 	TreeWriter *tw = new TreeWriter( inputTree, outputFileName, 0 );
+	bool isData = tw->isData();
+
 	// common settings
 	tw->SetProcessNEvents(-1);
 	tw->SetReportEvents(20000);
 
-	if( isMC ) {
-		tw->PileUpWeightFile("pileUpReweighting/puWeights.root");
-	} else {
-		tw->IncludeAJson("../../Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt");
+	if( isData ) {
+		std::cout << "Run over data." << std::endl;
+		tw->IncludeAJson( "../../Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt" );
+		//tw->IncludeAJson( "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt" );
 		std::vector<const char*> triggerNames;
 		triggerNames.push_back( "HLT_Photon70_CaloIdXL_PFHT400_v" );
 		triggerNames.push_back( "HLT_Photon70_CaloIdXL_PFNoPUHT400_v" );
 		tw->SetTriggerPaths( triggerNames );
+	} else {
+		std::cout << "Run over simulation." << std::endl;
+		tw->PileUpWeightFile("pileUpReweighting/puWeights.root");
 	}
 
 	double start_time = time(NULL);
