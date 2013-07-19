936b1d2d5a207e754534edc5497afb7e20d7b6d4
Knut <maximilian.kiesel@rwth-aachen.de>
2013-07-19 16:30:52 +0200

    ptJet a fuction now

--- TreeWriter/TreeObjects.cc
+++ TreeWriter/TreeObjects.cc
@@ -1,5 +1,12 @@
 #include "TreeObjects.h"
 
+float tree::Photon::ptJet() const {
+	if( _ptJet )
+		return _ptJet;
+	else
+		return pt;
+}
+
 bool tree::EtGreater(const tree::Particle p1, const tree::Particle p2) {
 	return p1.pt > p2.pt;
 }
