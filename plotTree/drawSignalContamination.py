3fe20cf16785faed24a17509b50d2fd97b0c8be1
Knut <maximilian.kiesel@rwth-aachen.de>
2013-09-11 12:13:19 +0200

    using global ewk fake rate

--- plotTree/drawSignalContamination.py
+++ plotTree/drawSignalContamination.py
@@ -4,69 +4,54 @@
 from multiplot import Multihisto
 from treeFunctions import *
 
-def applyFakeRate( histo, f, e_f ):
-	for i in range( histo.GetNbinsX() +1 ):
-		binContent = histo.GetBinContent(i)
-		histo.SetBinContent( i, histo.GetBinContent(i)*f)
-		# sigma_{ef} = sqrt( ( e*e_f )**2 + ( e_e*f )**2 )
-		histo.SetBinError( i, sqrt((binContent*e_f)**2 + (histo.GetBinError(i)*f)**2 ) )
-	return histo
+def drawSignalContamination( filename, xy, split ):
+	import array
+	label, unit, binning = readAxisConf("met")
+	metBinning = array.array( "d", binning )
 
-def drawSignalContamination( filename, plot ):
+	gHisto = readHisto( filename, "gMet%s_%s"%xy ).Rebin(len(metBinning)-1, randomName(), metBinning )
+	eHisto = readHisto( filename, "eMet%s_%s"%xy ).Rebin(len(metBinning)-1, randomName(), metBinning )
+	fHisto = readHisto( filename, "fMet%s_%s"%xy ).Rebin(len(metBinning)-1, randomName(), metBinning )
 
-	plot = "met"
-
-	###########################################################
-	fakeRate = 0.0084
-	fakeRateError = 0.0006
-
-	# correct fake rate, if it is estimated with yutaros method
-	fakeRateError = fakeRateError / (1-fakeRate)**2
-	fakeRate = fakeRate / ( 1 - fakeRate )
-
-	weight2D = readHisto( "qcdWeight.root", "qcdWeight" )
-	###########################################################
-
-	can = ROOT.TCanvas()
-	can.cd()
-	can.SetLogy(False)
-
-	gTree = readTree( filename, "photonTree" )
-	gHisto = getHisto( gTree, plot )
-
-	eTree = readTree( filename, "photonElectronTree" )
-	eHisto = applyFakeRate( getHisto( eTree, plot, color=2 ), fakeRate, fakeRateError )
+	eHisto = applyFakeRateEWK( eHisto )
 	eContamination = divideHistos( eHisto, gHisto )
-	eContamination.GetYaxis().SetTitle("signal contamination")
+	eContamination.SetLineColor(2)
 
-	fTree = readTree( filename, "photonJetTree" )
-	from qcdClosureSingleFile import writeWeight2DToFile
-	writeWeight2DToFile( filename, fTree, weight2D, "foWeights" )
-	fTree.AddFriend( "foWeights", filename )
-	fHisto = getHisto( fTree, plot, color=4, weight="weight*w_qcd" )
 	fContamination = divideHistos( fHisto, gHisto )
+	fContamination.SetLineColor(4)
 
 	totalContamination = divideHistos( addHistos( [eHisto, fHisto] ), gHisto )
 	totalContamination.SetLineColor(1)
 	totalContamination.SetMarkerColor(1)
 
-	mh = Multihisto()
-	mh.leg.SetHeader("Prediction from")
-	mh.addHisto( eContamination, "#gamma_{e}", draw="e0" )
-	mh.addHisto( fContamination, "#gamma_{jet}", draw="e0" )
-	mh.addHisto( totalContamination, "total", draw="e0" )
-	mh.Draw()
+	for h in [totalContamination, fContamination, eContamination]:
+		h.GetYaxis().SetTitleOffset( 1.85 )
+		h.SetTitle(";%s [%s];signal contamination"%(label,unit))
+		h.SetLineWidth(2)
 
-	SaveAs(can, "signalContamination_%s"%getDatasetAbbr( filename ) )
+	mh = Multihisto()
+	if split:
+		mh.addHisto( totalContamination, "Total", draw="e0" )
+		mh.addHisto( eContamination, "#gamma_{e}", draw="e0" )
+		mh.addHisto( fContamination, "#gamma_{jet}", draw="e0" )
+	else:
+		mh.addHisto( totalContamination, "", draw="e0" )
+
+	can = ROOT.TCanvas()
+	can.cd()
+	can.SetLogy(False)
+	mh.Draw()
+	SaveAs(can, "signalContamination_%s_%s_%s_%s"%(xy+(split,filename[0:-4] )))
 
 
 
 if __name__ == "__main__":
 	arguments = argparse.ArgumentParser()
 	arguments.add_argument("filenames", nargs="+", type=isValidFile )
-	arguments.add_argument("--plot", nargs="+", default = ["met"] )
+	arguments.add_argument("--xy", nargs=2, default = [1200,1220], type=int )
+	arguments.add_argument("--split", action="store_true" )
 	opts = arguments.parse_args()
 
 	for inName in opts.filenames:
-		drawSignalContamination( inName, opts.plot )
+		drawSignalContamination( inName, tuple(opts.xy), opts.split )
 
