57165125e0b3be78d919bad85944badd4c1c078e
knut <kiesel@cern.ch>
2013-11-15 07:26:23 -0600

    changed input paths for some files

--- TreeWriter/executable.cc
+++ TreeWriter/executable.cc
@@ -19,10 +19,9 @@ int main( int argc, char** argv ) {
 	tw->SetPhotonPtThreshold( 80 );
 	tw->ApplyHadronicSelection( true );
 
-	tw->SetQcdWeightFile("../plotTree/qcdWeight.root");
-	tw->SetJsonFile( "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Reprocessing/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt" );
-	tw->SetJsonFile( "../../Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt" );
-	tw->SetPileUpWeightFile( "pileUpReweighting/puWeights.root" );
+	tw->SetQcdWeightFile("qcdWeight.root");
+	tw->SetJsonFile( "Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt" );
+	tw->SetPileUpWeightFile( "puWeights.root" );
 
 	std::vector<const char*> triggerNames;
 	triggerNames.push_back( "HLT_Photon70_CaloIdXL_PFHT400_v" );
