6e89fcaddb7451135d43db138f7acbe28b5fe421
Knut Kiesel <kiesel@cern.ch>
2014-11-07 08:45:26 +0100

    writing also last enries to file

--- SplitTree/main.cc
+++ SplitTree/main.cc
@@ -28,21 +28,20 @@ int main( int argc, char** argv ) {
     std::string basename = infilename;
     basename = basename.substr( 0, basename.size()-5 );
 
-
-    TChain* oldTree = new TChain( "susyTree" );
-    oldTree->Add( infilename );
+    TChain oldTree( "susyTree" );
+    oldTree.Add( infilename );
 
     // object to read
-    std::vector<std::string>* gridParamStr =0;
-    oldTree->SetBranchAddress( "gridParamStr", &gridParamStr );
+    std::vector<std::string>* gridParamStr = 0;
+    oldTree.SetBranchAddress( "gridParamStr", &gridParamStr );
 
     std::string currentModel = "";
     std::string oldModel = "";
     TFile* newFile;
     TTree* newTree;
 
-    for( int i=0; i<oldTree->GetEntries(); i++) {
-        oldTree->GetEntry(i);
+    for( int i=0; i<oldTree.GetEntries(); i++) {
+        oldTree.GetEntry(i);
         if( gridParamStr->size() != 2 ) {
             std::cout << "ERROR: gridParamStr must be 2" << std::endl;
             continue;
@@ -51,15 +50,16 @@ int main( int argc, char** argv ) {
         if( currentModel.empty() ) std::cout << "ERROR: Something went wrong with extracting grid point" << std::endl;
 
         if( currentModel != oldModel ) {
+            std::cout << "New model: " << currentModel << std::endl;
             oldModel = currentModel;
             if( i ) newFile->Write(); // Write oldModel to file, but not for first entry
             newFile = new TFile( (basename+"_"+currentModel+".root").c_str(), "NEW" ); // or maybe substr from in put file
-            newTree = oldTree->CloneTree(0);
+            newTree = oldTree.CloneTree(0);
         }
         newTree->Fill();
     }
+    newFile->Write();
     delete gridParamStr;
-    delete oldTree;
     delete newTree;
     delete newFile;
 }
