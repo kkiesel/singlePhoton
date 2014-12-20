1073822124bd0253ee39127ac244304770f0b135
knut <kiesel@cern.ch>
2014-12-20 06:46:44 +0100

    get part of filename

--- SplitTree/main.cc
+++ SplitTree/main.cc
@@ -1,10 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
 #include "TChain.h"
 #include "TTree.h"
 #include "TFile.h"
-#include <string>
-#include <iostream>
-#include <vector>
-#include <stdlib.h>
+#include "TPRegexp.h"
 
 std::string getModelFromGridParamStr( const std::vector<std::string>& gridParamStr ) {
     if( gridParamStr.size() < 2 ) return "";
@@ -31,9 +33,10 @@ std::string random_string( const int len ) {
 
 int main( int argc, char**argv ) {
 
-    // set new random seed
+    // set new random seed for random names
     srand(time(0));
 
+    // Set error level
     //gErrorIgnoreLevel = kPrint, kInfo, kWarning, kError, kBreak, kSysError, kFatal;
 
     if( argc != 2 ) {
@@ -42,8 +45,18 @@ int main( int argc, char**argv ) {
     }
     std::cout << "Inputfilename: " << argv[1] << std::endl;
 
+    // Find suffix of input file (will be appended to output filename)
+    TPRegexp expFilename( ".*susyEvents(.*).root" );
+    TObjArray *arr = expFilename.MatchS( argv[1] );
+    std::string inputFileSuffix("");
+    if( arr->GetLast() > 0 ){
+        inputFileSuffix = (std::string)(((TObjString *)arr->At(1))->GetString());
+    }
+
+    // Read input tree
+    std::string dCachePrefix( "dcap://dcache-cms-dcap.desy.de" );
     TChain oldTree( "susyTree" );
-    if( !oldTree.Add( argv[1] ) ) {
+    if( !oldTree.Add( (dCachePrefix+argv[1]).c_str() ) ) {
         std::cout << "File could not be opened or tree could not be found" << std::endl;
         return 0;
     }
@@ -57,6 +70,7 @@ int main( int argc, char**argv ) {
     TFile* newFile;
     TTree* newTree;
 
+    // Start the event loop
     for( int i=0; i<oldTree.GetEntries(); i++) {
         oldTree.GetEntry(i);
         if( gridParamStr->size() != 2 ) {
@@ -76,7 +90,7 @@ int main( int argc, char**argv ) {
                 std::cout << "Beginning with point: " << currentModel << std::endl;
             }
 
-            newFile = new TFile( (currentModel+"_"+random_string(9)+".root").c_str(), "NEW" ); // or maybe substr from in put file
+            newFile = new TFile( ("/nfs/dust/cms/user/kiesel/tmp/"+currentModel+inputFileSuffix+"_"+random_string(9)+".root").c_str(), "NEW" ); // or maybe substr from in put file
             newTree = oldTree.CloneTree(0);
             oldModel = currentModel;
         }
